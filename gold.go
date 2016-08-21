6bb2b6069006bd12ef5f9a5521d76368bfe73deb
Felix Neumann <dev@fxnn.de>
2016-08-21 19:04:56 +0200

    gold.go: source arg

--- gold.go
+++ gold.go
@@ -1,9 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var source = flag.String("source", "", "Path to source CSV file")
 
 func main() {
 
-	fmt.Sprintf("gold")
+	flag.Parse()
+
+	fmt.Printf("gold\n")
+	fmt.Printf("source: %s\n", *source)
+	fmt.Println()
+
+	if *source == "" {
+		fmt.Printf("Error: No source given\n")
+		os.Exit(1)
+	}
 
 }
